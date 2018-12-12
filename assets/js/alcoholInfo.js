427ad0339335d8fdd0ea33af88fcd60082b27144
Unknown <Goldbeauti8@gmail.com>
2018-12-12 12:47:21 +0100

    About and Game site done

--- assets/js/alcoholInfo.js
+++ assets/js/alcoholInfo.js
@@ -2,38 +2,38 @@ $(document).ready(function () {
 
     $(".carousel-control-next").click(function () {
         if ($(".carousel-inner div:nth-child(3)").hasClass('active')) {
-            if ($('.jumbotron p:first-child').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:first-child').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:first-child').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:first-child').removeClass('hideLead').addClass('lead');
             }
         } else if ($(".carousel-inner div:nth-child(2)").hasClass('active')) {
-            if ($('.jumbotron p:nth-child(3)').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:nth-child(3)').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:nth-child(3)').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:nth-child(3)').removeClass('hideLead').addClass('lead');
             }
         } else if ($(".carousel-inner div:first-child").hasClass('active')) {
-            if ($('.jumbotron p:nth-child(2)').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:nth-child(2)').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:nth-child(2)').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:nth-child(2)').removeClass('hideLead').addClass('lead');
             }
         }
     });
 
     $(".carousel-control-prev").click(function () {
         if ($(".carousel-inner div:nth-child(2)").hasClass('active')) {
-            if ($('.jumbotron p:first-child').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:first-child').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:first-child').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:first-child').removeClass('hideLead').addClass('lead');
             }
         } else if ($(".carousel-inner div:nth-child(3)").hasClass('active')) {
-            if ($('.jumbotron p:nth-child(2)').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:nth-child(2)').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:nth-child(2)').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:nth-child(2)').removeClass('hideLead').addClass('lead');
             }
         } else if ($(".carousel-inner div:first-child").hasClass('active')) {
-            if ($('.jumbotron p:nth-child(3)').hasClass('hideLead')) {
-                $('.jumbotron p.lead').removeClass('lead').addClass('hideLead');
-                $('.jumbotron p:nth-child(3)').removeClass('hideLead').addClass('lead');
+            if ($('.jumbotron div:nth-child(3)').hasClass('hideLead')) {
+                $('.jumbotron div.lead').removeClass('lead').addClass('hideLead');
+                $('.jumbotron div:nth-child(3)').removeClass('hideLead').addClass('lead');
             }
         }
     });
